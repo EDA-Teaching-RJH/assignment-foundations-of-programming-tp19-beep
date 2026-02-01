0288db825be26444e63883c39ac9358b6c2127d2
tp19-beep <tp19@kent.ac.uk>
2026-02-01 18:31:06 +0000

    line 41 - 43

--- old_system.py
+++ old_system.py
@@ -39,6 +39,8 @@ def run_system_monolith():
             
            
             n.append(new_name)
+            r.append(new_rank)
+            d.append(new_div)
             print("Crew member added.")
             
         elif opt == "3":
