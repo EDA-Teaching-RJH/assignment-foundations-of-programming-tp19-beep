54d9d79b308abac6ba197be1068538a79a76bafd
tp19-beep <tp19@kent.ac.uk>
2026-02-04 16:43:26 +0000

    display_menu

--- fleet_manager.py
+++ fleet_manager.py
@@ -6,4 +6,22 @@ def init_database():
 
     return names, ranks, divs, ids
 
+def display_menu():
+    full_name = input("FULL NAME:")
+    print("HELLO ", full_name)
+
+    print("MENU:")
+    print("1. Add Member")
+    print("2. Remove Member")
+    print("3. Update Rank")
+    print("4. Display Roster")
+    print("5. Search Crew")
+    print("6. Filter By Divisions")
+    print("7. Calculate Payroll")
+    print("8. Count Officers")
+
+    option = input("Select an Option:")
+
+    return option
     
+
