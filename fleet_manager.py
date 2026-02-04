1ba14e86d002d71b6470f06e719bbbfec3ad8eef
tp19-beep <tp19@kent.ac.uk>
2026-02-04 00:32:00 +0000

    init_database

---
+++ fleet_manager.py
@@ -0,0 +1,7 @@
+def init_database():
+    names = ["Deanna Troi", "Wesley Crusher", "William Riker", "Liam Shaw", "T'Veen"]
+    Ranks = ["Lieutenant Commander", "Lieutenant", "Commander", "Captain"]
+    Divisions = ["Diplomatic Officer", "Engineering Officer", "First Officer", "Commanding Officer", "Science Officer"]
+    IDs = ["101", "102", "103", "104", "105"]
+
+    
