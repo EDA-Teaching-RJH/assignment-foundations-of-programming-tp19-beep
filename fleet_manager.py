defd1f960ef194a22495a23afef00b7b4216eb71
tp19-beep <tp19@kent.ac.uk>
2026-02-04 13:52:51 +0000

    init_database2

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,7 +1,9 @@
 def init_database():
     names = ["Deanna Troi", "Wesley Crusher", "William Riker", "Liam Shaw", "T'Veen"]
-    Ranks = ["Lieutenant Commander", "Lieutenant", "Commander", "Captain"]
-    Divisions = ["Diplomatic Officer", "Engineering Officer", "First Officer", "Commanding Officer", "Science Officer"]
-    IDs = ["101", "102", "103", "104", "105"]
+    ranks = ["Lieutenant Commander", "Lieutenant", "Commander", "Captain", "Ensign"]
+    divs = ["Command", "Operations", "Command", "Command", "Sciences",]
+    ids = [101, 102, 103, 104, 105]
+
+    return names, ranks, divs, ids
 
     
